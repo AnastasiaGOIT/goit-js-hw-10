62e295c9ae4c877653f82195f35703b27412ecd1
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-09-29 19:28:47 +0300

    m

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+// import axios from 'axios';
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_7rQ0eMZKqrdlo58gDOF1t115hunRXcwNChu7oi4nQ9ehNnK6T7reiR7OXWhR6ay';
+
+export function fetchBreeds() {
+  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  ).then(response => {
+    // console.log(response);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_7rQ0eMZKqrdlo58gDOF1t115hunRXcwNChu7oi4nQ9ehNnK6T7reiR7OXWhR6ay';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const select = document.querySelector('.breed-select');
+const info = document.querySelector('.cat-info');
+// console.log(info);
+
+fetchBreeds()
+  .then(breeds =>
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.textContent = breed.name;
+      option.value = breed.id;
+      select.appendChild(option);
+    })
+  )
+  .catch(err => console.log(err));
+
+select.addEventListener('change', onChange);
+function onChange(e) {
+  const breedId = e.target.value;
+
+  fetchCatByBreed(breedId)
+    .then(cat => markUpFunction(cat))
+    .catch(err => console.log(err));
+
+  function markUpFunction(cat) {
+    const markUp = `
+ <img
+  src="${cat.url}"
+  alt="${cat.name}"
+/>
+<h2>${cat.name}</h2>
+<p>${cat.description}</p>
+<p>${cat.temperament}</p>`;
+    info.innerHTML = markUp;
+  }
+}
