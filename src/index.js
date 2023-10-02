7adc8b725248b079138ce1f0c47d99b52a51128d
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-10-02 12:00:11 +0300

    bb

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import axios from 'axios';
+import SlimSelect from 'slim-select';
+new SlimSelect({
+  select: 'select',
+});
 
-// import SlimSelect from 'slim-select';
-// new SlimSelect({
-//   select: 'select',
-// });
 import Notiflix from 'notiflix';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_JlpgD5LjMPXqnfbdBzuobVXBrTl7X1HSYzZlZGsvu1aANVyPApJiTRp2NpcoLvGo';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const select = document.querySelector('.breed-select');
+// const select = document.querySelector('.breed-select');
 const info = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
+function wrapper() {
+  loader.style.display = 'block';
 
-fetchBreeds()
-  .then(breeds => {
-    const options = breeds.map(breed => {
-      const optionEl = document.createElement('option');
-      optionEl.textContent = breed.name;
-      optionEl.value = breed.id;
-      return optionEl;
-    });
+  fetchBreeds()
+    .then(breeds => {
+      const options = createOptions(breeds);
 
-    select.append(...options);
-  })
-  .catch(err => console.log(err));
-error.style.display = 'none';
-loader.style.display = 'none';
+      select.append(...options);
+    })
+    .catch(err => console.log(err))
+    .finally((loader.style.display = 'none'), (error.style.display = 'none'));
+}
+wrapper();
 // loader.classList.add('none');
 
 select.addEventListener('change', onChange);
@@ -43,25 +37,38 @@ function onChange(e) {
 
   fetchCatByBreed(breedId)
     .then(data => {
-      const [cat] = data;
-      info.style.display = 'none';
-      //   info.classList.add('none');
-      info.innerHTML = `<img
-   src="${cat.url}"
-   alt="${cat.breeds[0].name}" width='300'
- />
- <h2>${cat.breeds[0].name}</h2>
- <p>${cat.breeds[0].description}</p>
- <p>${cat.breeds[0].temperament}</p>`;
-      //   info.classList.remove('none');
-      info.style.display = 'block';
-      select.style.display = 'block';
-      loader.style.display = 'none';
+      createMarkup(data);
       //   loader.classList.add('none');
     })
     .catch(() => {
       //   info.classList.add('none');
       info.style.display = 'none';
       Notiflix.Report.failure('Title', 'Message', 'Button Text');
-    });
+    })
+    .finally((info.style.display = 'none'), (loader.style.display = 'block'));
+}
+
+function createOptions(breeds) {
+  return breeds.map(breed => {
+    const optionEl = document.createElement('option');
+    optionEl.textContent = breed.name;
+    optionEl.value = breed.id;
+    return optionEl;
+  });
+}
+function createMarkup(data) {
+  const [cat] = data;
+  info.style.display = 'none';
+  //   info.classList.add('none');
+  info.innerHTML = `<img
+   src="${cat.url}"
+   alt="${cat.breeds[0].name}" width='300'
+ />
+ <h2>${cat.breeds[0].name}</h2>
+ <p>${cat.breeds[0].description}</p>
+ <p>${cat.breeds[0].temperament}</p>`;
+  //   info.classList.remove('none');
+  info.style.display = 'block';
+  select.style.display = 'block';
+  loader.style.display = 'none';
 }
